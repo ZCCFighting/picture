9f354b87a4a528c43e5bdfdbeadd06f96d9145d5
zccmmc <1174877450@qq.com>
2019-02-01 15:57:10 +0800

    wison

--- buildingget.py
+++ buildingget.py
@@ -5,8 +5,11 @@ import re
 import exifread
 import gdal
 import numpy as np
-import skimage.io
-
+#import skimage.io
+import gdal
+import os
+import numpy as np
+'''
 def latitude_and_longitude_convert_to_decimal_system(*arg):
     """
     经纬度转为小数, 作者尝试适用于iphone6、ipad2以上的拍照的照片，
@@ -14,6 +17,7 @@ def latitude_and_longitude_convert_to_decimal_system(*arg):
     :return: 十进制小数
     """
     return float(arg[0]) + ((float(arg[1]) + (float(arg[2].split('/')[0]) / float(arg[2].split('/')[-1]) / 60)) / 60)
+
 
 def find_GPS_image(pic_path):
     GPS={}
@@ -46,30 +50,33 @@ def find_GPS_image(pic_path):
             elif re.match('.*Date.*', tag):
                 date = str(value)
     return {'GPS_information': GPS, 'date_information': date}
+'''
 def imageread(pic_path):
     GPS={}
     date=''
     f=open(pic_path,'rb')
     imagetext=exifread.process_file(f)
-    #for key in imagetext:
-        #print(key,":",imagetext[key])
+    for key in imagetext:
+        print(key,":",imagetext[key])
     print('************************\n************************')
     for q in imagetext:
         if q=="GPS GPSLatitude":
             print("GPS经度=",imagetext[q],imagetext['GPS GPSLatitudeRef'])
         elif q=="GPS GPSLongitude":
             print("GPS纬度=",imagetext[q],imagetext['GPS GPSLongitudeRef'])
-            print(type(imagetext[q]))
         elif q=="GPS GPSAltitude":
             print("GPS高度=",imagetext[q])           
         elif q=='Image DateTime':
             print("拍摄时间=",imagetext[q])
+
 def get_pic_size(pic_path):
     img=cv.imread(pic_path)
     sp=img.shape
     Pheignt=sp[0]
     Pwidth=sp[1]
-    return{'Pheight':Pheignt,'Pwidth':Pwidth}
+    Pdeep=sp[2]
+    return{'Pheight':Pheignt,'Pwidth':Pwidth,'Pdeep':Pdeep}
+
 def get_video_size(video_path):
     cap = cv.VideoCapture(video_path)
     width=int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
@@ -89,9 +96,47 @@ def get_video_size(video_path):
     cv.destroyAllWindows()
     #return {'video_height':height,'video_width':width,'video_fps':fps}
 
+def get_pic_coord(pic_coord_in,pic_coord_out):
+    infile=pic_coord_in
+    out_file=pic_coord_out
+    if os.path.splitext(infile[-1]=='.tif') and os.path.splitext(out_file[-1]=='.tif'):
+        gdal.AllRegister()
+        in_pic=gdal.open(infile,gdal.GA_ReadOnly)
+        out_pic=gdal.open(out_file,gdal.GA_Update)
+        srs=in_pic.GetProjectionRef()
+        out_pic.SetProjection(srs)
+        geotransform=in_pic.GetGeoTransform()
+        out_pic.SetGeoTransform(geotransform)
+        out_pic.FlushCache()
+        del in_pic,out_pic,infile
+
+def pic_connect(pic_path1,pic_path2,outfile,a):
+    img1=cv.imread(pic_path1)
+    img2=cv.imread(pic_path2)
+    pc1_size=get_pic_size(pic_path1)
+    print(pc1_size)
+    pc2_size=get_pic_size(pic_path2)
+    print(pc2_size)
+    if(a==0): #a=0表示按行
+        img3=np.concatenate([img1, img2], axis=1)
+        cv.imwrite(outfile,img3)
+    elif (a==1):#表示按列连接
+        img3=np.concatenate([img1,img2],axis=0)
+        cv.imwrite(outfile,img3)
+    else:
+        print('The two pictures can not be connected')
+        return
+    
 
 
-print(start)
-show=get_video_size(0)
+
+
+
+
+
+
+#show=get_video_size(0)
+#GPSinformations=find_GPS_image('test.JPG')
 #imageread('test.JPG')
-print(show)
+pic_connect('pictures/1_01.tif','pictures/2_01.tif','out1.tif',1)
+#print(show)
